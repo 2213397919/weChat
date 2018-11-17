08381bb36be49f87d85c11696484d6ef0113de1a
zhangshun <2213397918@qq.com>
2018-11-17 21:12:54 +0800

    完成了获取access-token操作

--- Tool/weChat/weChat.js
+++ Tool/weChat/weChat.js
@@ -12,10 +12,17 @@ class weChat {
         //发送请求
         const result = await rp({method:'GET',url,json:true});
         //设置access_token的过期时间, 提前5分钟刷新
-        result.expires_in = Date.now() + 7200000 - 300000;
+        result.expires_in = Date.now() + 6900000;
         //返回result
         return result;
     }
+    /**
+     * 保存access_token
+     * @param filePath  要保存的文件路径
+     * @param accessToken  要保存的凭据
+     * @return {Promise<any>}
+     */
+    //将得到的AccessToken保存下来。
      saveAccessToken(filePath,accessToken){
         return new Promise((resolve,reject) =>{
             writeFile(filePath,JSON.stringify(accessToken),err =>{
@@ -27,10 +34,12 @@ class weChat {
             })
         })
     }
+    //读取AccessToken。
      readAccessToken(filePath){
         return new Promise((resolve,reject) =>{
             readFile(filePath,(err,data) =>{
                 if (!err){
+                    //读取的是buffer，需要转换成js对象。
                     resolve(JSON.parse(data.toString()));
                 } else {
                     reject('readAccessToken:'+err);
@@ -38,33 +47,53 @@ class weChat {
             })
         })
     }
-
+    //检测AccessToken有没有过期。
     isValidAccessToken ({expires_in}) {
+        //过期返回false,没有过期返回true
         return Date.now() < expires_in;
     }
+    //返回有效access_token的方法
+    fetchAccessToken(){
+        if (this.access_token && this.expires_in && this.isValidAccessToken(this)){
+            console.log('获取到啦');
+            //说明access_token是有效的，返回this.access_token（）和this.expires_in（过期时间）
+            return Promise.resolve({access_token: this.access_token, expires_in: this.expires_in});
+        }
+        ////读取本地AccessToken成功，然后判断一下有没有过期。
+        return this.readAccessToken('./accessToken.txt')
+            .then(async res =>{
+                if (this.isValidAccessToken(res)){
+                    //没有过期，直接使用。作为then函数返回值， promise对象包着res
+                    return res;
+                }else {
+                    //过期，重新获取AccessToken，并在本地保存。
+                    const accessToken = await this.getAccessToken();
+                    await this.saveAccessToken('./accessToken.txt', accessToken);
+                    return accessToken;
+                }
+            })
+            //读取本地AccessToken失败，重新获取AccessToken，并在本地保存。
+            .catch(async err =>{
+                const accessToken = await this.getAccessToken();
+                await this.saveAccessToken('./accessToken.txt', accessToken);
+                return accessToken
+            })
+            //第一个.then里面return res,返回的是一个promise对象。
+            .then(res => {
+                //不管上面成功或者失败都会来到这
+                this.access_token = res.access_token;
+                this.expires_in = res.expires_in;
 
+                return Promise.resolve(res);
+            })
+    }
 }
-
+//声明一个自执行函数，来操作AccessToken。
 (async () =>{
     //实例化对象
     const w = new weChat();
-    //声明文件的路径
-    w.readAccessToken('./accessToken.txt')
-        //判断有没有过期，没有过期直接使用，如果过期，需要重新获取，并且保存。
-        .then(async res => {
-            if (w.isValidAccessToken(res)) {
-                //没有过期，直接使用
-                console.log(res);
-                console.log('没有过期，直接使用');
-            } else {
-                //过期了
-                const accessToken = await w.getAccessToken();
-                await w.saveAccessToken('./accessToken.txt', accessToken);
-            }
-        })
-        //如果出现错误，获取AccessToken，然后保存下来。
-        .catch(async err => {
-            const accessToken = await w.getAccessToken();
-            await w.saveAccessToken('./accessToken.txt', accessToken);
-        })
+    let result = await w.fetchAccessToken();
+    console.log(result);
+    // result = await w.fetchAccessToken();
+    // console.log(result);
 })();
