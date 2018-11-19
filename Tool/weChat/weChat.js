80fdbb4a88e7adc445a99b95614dba0d79079b04
zhangshun <2213397918@qq.com>
2018-11-19 15:54:40 +0800

    完成了微信的网页开发的逻辑搭建

--- Tool/weChat/weChat.js
+++ Tool/weChat/weChat.js
@@ -3,6 +3,7 @@ const rp = require('request-promise-native');
 const {writeFile,readFile} = require('fs');
 const {appID, appsecret} = require('../config');
 const api = require('../api');
+const {writeFileAsync,readFileAsync} = require('../list/tool');
 
 //class类
 class weChat {
@@ -90,6 +91,78 @@ class weChat {
                 return Promise.resolve(res);
             })
     }
+
+    /**
+     * 获取ticket
+     * @returns {Promise<{ticket: string, ticket_expires_in: number}>}
+     */
+    //获取ticket
+    async getTicket (){
+        //获取access_token
+        const {access_token} = await this.fetchAccessToken();
+        //定义请求地址
+        const url = `${api.ticket}access_token=${access_token}`;
+        //发送请求
+        const result = await rp({method:'GET',url,json:true});
+        //设置access_token的过期时间, 提前5分钟刷新
+        result.expires_in = Date.now() + 6900000;
+        //返回result
+        return {
+            ticket : result.ticket,
+            ticket_expires_in : Date.now()+6900000
+        }
+    }
+    //将得到的ticket保存下来。
+    saveTicket(filePath,ticket){
+        return writeFileAsync(filePath,ticket);
+    }
+    //读取ticket。
+    readTicket(filePath){
+        return readFileAsync(filePath);
+    }
+    //检测ticket有没有过期。
+    isValidTicket ({ticket_expires_in}) {
+        //过期返回false,没有过期返回true
+        return Date.now() < ticket_expires_in;
+    }
+    //返回有效ticket的方法
+    fetchTicket(){
+        //如果ticket是有效的，直接返回，不用执行下面的逻辑。
+        if (this.ticket && this.expires_in && this.isValidAccessToken(this)){
+            console.log('获取到啦');
+            //说明access_token是有效的，返回this.access_token（）和this.expires_in（过期时间）
+            return Promise.resolve({ticket: this.ticket, ticket_expires_in: this.ticket_expires_in});
+        }
+        ////读取本地AccessToken成功，然后判断一下有没有过期。
+        return this.readTicket('./ticket.txt')
+            .then(async res =>{
+                if (this.isValidTicket(res)){
+                    //没有过期，直接使用。作为then函数返回值， promise对象包着res
+                    return res;
+                }else {
+                    //过期，重新获取AccessToken，并在本地保存。
+                    const ticket = await this.getTicket();
+                    await this.saveTicket('./ticket.txt', ticket);
+                    //作为then函数返回值， 返回promise对象。
+                    return ticket;
+                }
+            })
+            //读取本地AccessToken失败，重新获取AccessToken，并在本地保存。
+            .catch(async err =>{
+                const ticket = await this.getTicket();
+                await this.saveAccessToken('./ticket.txt', ticket);
+                //作为then函数返回值， 返回promise对象。
+                return ticket
+            })
+            //不管上面成功或者失败都会来到这
+            .then(res => {
+                this.ticket = res.ticket;
+                this.ticket_expires_in = res.ticket_expires_in;
+
+                return Promise.resolve(res);
+            })
+    }
+
     /**
      * 创建自定义菜单
      * @param menu
@@ -144,7 +217,7 @@ class weChat {
             //获取请求地址
             const url = `${api.tag.create}access_token=${access_token}`;
             //发送请求
-            const result = await rp({method: 'POST', url, json: true, body: {'tag': {'name':name}}});
+            const result = await rp({method: 'POST', url, json: true, body: {tag: {name}}});
             return result;
         }catch (e) {
             return console.log('createTag'+e);
@@ -200,7 +273,7 @@ class weChat {
     result = await w.createMenu(require('./menu'));
     console.log(result);
     //用户管理
-    let createTags = await w.createTag('璀璨星光');
+    let createTags = await w.createTag('璀璨星光者');
     console.log(createTags);
     let createTags1 = await w.batchUsersTag([
         'orvpe1YBxSXdkGybDAXKrrZFlSmk',
